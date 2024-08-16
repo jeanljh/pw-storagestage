0211efd523e0adbf5880492661c164e0868732e6
jean <leejeanhwa@gmail.com>
2024-08-16 18:29:26 +0800

    update locator

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '../fixtures/base'
 
 test('sign in with storage state file if present', async ({ page }) => {
-  await page.getByText(/Welcome to Teams!/).waitFor()
+  await page.locator('[data-tid="chat-list-header"]').waitFor()
 });
