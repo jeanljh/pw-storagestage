05a94e4c4d75113984a28262d97ad3e815e10045
jean <leejeanhwa@gmail.com>
2024-08-20 14:33:13 +0800

    update test script

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '../fixtures/base'
 
 test('sign in with storage state file if present', async ({ page }) => {
+  await page.getByRole('button', { name: /Continue/ }).click()
   await page.locator('[data-tid="chat-list-header"]').waitFor()
 });
